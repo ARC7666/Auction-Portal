85e7cbce140c3951bab6d8a37370cd8d75619cef
Ankit Ranjan <ankitranjan766@gmail.com>
2025-06-04 23:31:57 +0530

    started working on buyer dashboard

--- client/src/pages/BuyerDashboard/buyer-dashboard.jsx
+++ client/src/pages/BuyerDashboard/buyer-dashboard.jsx
@@ -3,20 +3,38 @@ import './buyer-dashboard.css';
 
 function BuyerDashboard() {
   return (
-  <div className="dashboard-container">
-    <h1>Buyer Dashboard</h1>
-    <p>Welcome to your buyer portal.</p>
-      
-    <section className="dashboard-section">
-      <h3>Recent Bids</h3>
-      <p>[]</p>
-     </section>
+    
 
-    <section className="dashboard-section">
-      <h3>Recommended Products</h3>
-      <p>[]</p>
-    </section>
-  </div>
+    <div className="seller-dashboard">
+      <aside className="sidebar">
+        <div className="logo">
+          <div className="logo"><img src={logo} alt="Logo" /></div>
+        </div>
+
+        <div className="dashboard-title">
+          <hr />
+          <span>Seller Dashboard</span>
+          <hr />
+        </div>
+
+        <nav className="nav-buttons">
+          <Link to="/create-auction"><button>Create Auction</button></Link>
+          <Link to="/seller-auctions"><button>View Listing</button></Link>
+          <Link to="/seller-analytics"><button>Sale Analytics</button></Link>
+          <Link to="/chats"><button>Chat</button></Link>
+        </nav>
+
+        <div className="logout-button">
+          <button onClick={handleLogout}>LOGOUT</button>
+        </div>
+      </aside>
+
+      <main className="dashboard-content">
+        <h1>Welcome Back {user?.displayName ? `“${user.displayName}”` : "!"}</h1>
+      </main>
+    </div>
+
+
   );
 }
 
